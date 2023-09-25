4c7113cc444203b92aecb1197552790b0a7b6222
Simamnkele <smambo85@gmail.com>
2023-09-25 19:37:49 +0200

    Added cocktail_sort

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * swap - swaps nodes in the list 
+ * @pre: previous (first) node
+ * @current: current (second) node
+ * @list: list with nodes to be swapped
+ * Return: nothing
+ */
+void swap(listint_t *pre, listint_t *current, listint_t **list)
+{
+	if (!(pre->prev))
+	{
+		pre->next = current->next;
+		if (current->next)
+			current->next->prev = pre;
+		current->next = pre;
+		current->prev = NULL;
+		pre->prev = current;
+		*list = current;
+	} else
+	{
+		current->prev->next = current->next;
+		if (current->next)
+			current->next->prev = current->prev;
+		pre->prev->next = current;
+		current->prev = pre->prev;
+		pre->prev = current;
+		current->next = pre;
+	}
+}
+
+/**
+ * cocktail_sort_list - sorts a doubly linked list of ints in
+ *            ascending order using Cocktail shaker sort algo
+ * @list: list to be sorted
+ * Return: nothing
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *current, *next_node;
+	int swop;
+
+	if (list == NULL || !(*list) || (*list)->next == NULL)
+		return;
+	current = (*list);
+	do {
+			swop = 0;
+			while (current->next)
+			{
+				next_node = current->next;
+				if (next_node && current->n > next_node->n)
+				{
+					swap(current, next_node, list);
+					swop = 1;
+					print_list((*list));
+				}
+				else
+					current = current->next;
+			}
+			current = current->prev;
+			while (current->prev)
+			{
+				next_node = current->prev;
+				if (next_node && current->n < next_node->n)
+				{
+					swap(next_node, current, list);
+					swop = 1;
+					print_list((*list));
+				}
+				else
+					current = current->prev;
+			}
+			current = current->next;
+	} while (swop);
+}
--- sort.h
+++ sort.h
@@ -29,5 +29,7 @@ void quick_sort(int *array, size_t size);
 int partition(int *array, int start, int end, int size);
 void recursive_quick_sort(int *array, int start, int end, int size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap(listint_t *pre, listint_t *current, listint_t **list);
 
 #endif /* SORT_H */
