7f76bd71b615750a3e4157e40bac38c3a7858e88
Simamnkele <smambo85@gmail.com>
2023-09-25 17:24:59 +0200

    Added shell_sort prototype

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int start, int end, int size);
 void recursive_quick_sort(int *array, int start, int end, int size);
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
